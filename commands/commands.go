55538f06d7e0db0ee5723bd4e35a4fd5deb69d9f
FedericoTorres233 <federicoivantorres1@gmail.com>
2023-11-27 23:57:00 +0000

    feat: add an array of commands

--- commands/commands.go
+++ commands/commands.go
@@ -4,6 +4,13 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
+var commands = [4]string{
+	"start",
+	"teams",
+	"msg",
+	"subscribe",
+}
+
 func Commands(b *tele.Bot) {
 	firstCommand(b)
 	get_teams(b)
