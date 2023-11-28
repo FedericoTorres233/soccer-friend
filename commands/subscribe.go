a3cb9dc2da292294705f8d60cb8d5b2bc03a6734
FedericoTorres233 <federicoivantorres1@gmail.com>
2023-11-28 00:32:34 +0000

    feat: create a map that stores user subscriptions

--- commands/subscribe.go
+++ commands/subscribe.go
@@ -1,16 +1,30 @@
 package commands
 
 import (
+	"strings"
+
 	tele "gopkg.in/telebot.v3"
-	"log"
 )
 
 func subscribe(b *tele.Bot) {
+	userSubs := make(map[int64][]string)
+
 	b.Handle("/subscribe", func(c tele.Context) error {
-		log.Println(c.Args())
-		if c.Args() == nil {
+
+		chatID := c.Chat().ID
+    arguments := c.Args()
+
+		// Update map to assign a chat to 1 or more teams
+		updatedUserSubs := append(userSubs[chatID], arguments...)
+		userSubs[chatID] = updatedUserSubs
+
+		// Check if arguments are nil
+		if arguments == nil {
 			return c.Send("Usage: /subscribe [team]")
 		}
-		return c.Send("Subscribed to " + c.Args()[0])
+
+		// If successful send message to user
+		return c.Send("Subscribed to " + strings.Join(arguments, ", "))
 	})
+
 }
